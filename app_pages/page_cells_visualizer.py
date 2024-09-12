35fee0e626a4ff29ebe4f9fbe590d4c1de9c7678
Jorgen Lovbakke <jorgenlovbakke@yahoo.co.uk>
2024-09-12 11:47:26 +0000

    Add correction to 'validation' link, line 44 in cells_visualizer

--- app_pages/page_cells_visualizer.py
+++ app_pages/page_cells_visualizer.py
@@ -41,7 +41,7 @@ def page_cells_visualizer_body():
     if st.checkbox("Image Montage"): 
       st.write("* To refresh the montage, click on the 'Create Montage' button")
       my_data_dir = 'inputs/malaria_dataset/cell_images'
-      labels = os.listdir(my_data_dir+ '/validation')
+      labels = os.listdir(my_data_dir + '/validation')
       label_to_display = st.selectbox(label="Select label", options=labels, index=0)
       if st.button("Create Montage"):      
         image_montage(dir_path= my_data_dir + '/validation',
